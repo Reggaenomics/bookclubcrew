9f7c973e5ec40ed66b92c1e067c84dc31d69cf4f
Reggaenomics <RyanKaufman425@gmail.com>
2023-05-19 04:17:39 -0500

    Bot response testing

--- app/Console/Commands/DiscordServe.php
+++ app/Console/Commands/DiscordServe.php
@@ -8,6 +8,7 @@ use App\Providers\EventServiceProvider;
 use Discord\Discord as DiscordClient;
 use Discord\DiscordCommandClient;
 use Discord\Parts\Channel\Message;
+use Discord\Parts\Channel\Channel;
 use Discord\WebSockets\Event;
 use Illuminate\Console\Command;
 use function React\Async\coroutine;
@@ -55,13 +56,40 @@ class DiscordServe extends Command
 
 
 
-        $discord->on('ready', function (DiscordClient $discord) {
-            foreach (Discord::getEvents() as $event => $listener) {
-                $discord->on($event, function($entity, $discord) use ($listener) {
+        $discord->on('ready', function (DiscordClient $discord) 
+        {
+            foreach (Discord::getEvents() as $event => $listener) 
+            {
+                $discord->on($event, function($entity, $discord) use ($listener) 
+                {
                     (new $listener())->handle($entity, $discord);
                 });
             }
+            $discord->on('message', function (Message $message, DiscordClient $discord) {
+                if ($message->content === 'test') 
+                {
+                    $channel = $message->channel;
+                    $author = $message->author;
+            
+                    $channel->sendMessage("Test successful. Fuck off, {$author->username}!");
+                } //This if statement works.
+
+            $discord->on('message', function (Message $message, DiscordClient $discord) {
+                if ($message->content === 'number' && !$message->author->bot) //This doesn't work
+                {
+                    $channel = $message->channel;
+                    $author = $message->author;
+                    $channel->sendMessage("Valid input, {$author->username}!");
+                }
+                else if (!$message->author->bot) //This doesn't work currently
+                {
+                    $channel = $message->channel;
+                    $author = $message->author;
+                    $channel->sendMessage("Invalid input, {$author->username}!");
+                }
+            });
         });
+    });
 
         $discord->run();
     }
